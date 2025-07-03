4e9dfca7d1e7b6379b7b3cd6a6f44a8a863077b0
shiva <shivarama311@gmai.com>
2025-07-03 11:45:52 +0530

    header active color

--- src/components/Slidebar/index.js
+++ src/components/Slidebar/index.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link ,useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import './index.css';
@@ -6,6 +6,7 @@ import './index.css';
 const Slidebar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+  const location = useLocation();
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
@@ -23,11 +24,11 @@ const Slidebar = () => {
 
         <div className={`collapse navbar-collapse ${isMobileMenuOpen ? 'show' : ''}`}>
           <ul className="navbar-nav ms-auto text-center">
-            <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
-            <li className="nav-item"><Link to="/plans" className="nav-link">Plans</Link></li>
-            <li className="nav-item"><Link to="/cost" className="nav-link">Cost</Link></li>
-            <li className="nav-item"><Link to="/terms" className="nav-link">Terms & Conditions</Link></li>
-            <li className="nav-item"><Link to="/contactUs" className="nav-link">Contact Us</Link></li>
+            <li className="nav-item"><Link to="/" className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}>Home</Link></li>
+            <li className="nav-item"><Link to="/plans" className={`nav-link ${location.pathname === '/plans' ? 'active' : ''}`}>Plans</Link></li>
+            <li className="nav-item"><Link to="/cost" className={`nav-link ${location.pathname === '/cost' ? 'active' : ''}`}>Cost</Link></li>
+            <li className="nav-item"><Link to="/terms" className={`nav-link ${location.pathname === '/terms' ? 'active' : ''}`}>Terms & Conditions</Link></li>
+            <li className="nav-item"><Link to="/contactUs" className={`nav-link ${location.pathname === '/contactUs' ? 'active' : ''}`}>Contact Us</Link></li>
           </ul>
         </div>
       </div>
